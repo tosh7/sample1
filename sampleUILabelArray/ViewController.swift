1cea6a2aaf0fc49b29f176715520310c9312662f
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-10-10 13:17:57 +0900

    first init

--- sampleUILabelArray/ViewController.swift
+++ sampleUILabelArray/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var labelArray:[UILabel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        for i in 0...4{
+            labelArray[i].text = "label number \(i)"
+        }
     }
 
 
